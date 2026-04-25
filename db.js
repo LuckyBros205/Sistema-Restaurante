f64976adf55475ad402214d3e78e6a545e61665e
Gustavo <luisgustavoquispe.205@gmail.com>
2026-04-25 14:27:39 -0500

    fix: usar mysql2 correctamente

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql'); // Usamos mysql seca, sin el 2
+const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 10240,
+    port: process.env.DB_PORT,
     ssl: {
         rejectUnauthorized: false // Esto es vital para Aiven
     }
